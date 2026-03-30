5fee3fd2bb51813151514980d20396faac94ed3b
Aarya Sarfare <aarya.work005@gmail.com>
2026-03-30 14:09:54 +0530

    successfully implemented the view all contacts endpoint for my API

--- app/routes.py
+++ app/routes.py
@@ -9,7 +9,7 @@ router = APIRouter()
 def root():
     return "Welcome to Contact Manager"
 
-@router.get("/contacts/{contact_name}", summary="Gets the contact entry by name")
+@router.get("/contacts/{contact_name}", summary = "Gets the contact entry by name")
 def get_one_contact_entry(contact_name: str):
     contact_number = op.view_one_contact_entry(contact_name = contact_name)
 
@@ -18,6 +18,11 @@ def get_one_contact_entry(contact_name: str):
     else:
         return {"contact_name": contact_name.title(), "contact_number": contact_number}
 
+@router.get("/contacts", summary = "Gets all the contact entries stored in the database")
+def get_all_contact_entries():
+
+    contacts_data = op.view_all_contacts()
+    return contacts_data
 
 @router.post("/contacts", summary="Create a new contact")
 def create_contact(contact: ContactEntry):
