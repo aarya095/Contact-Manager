6b11135ed2a165a0e64af916b1b08650a75a0407
Aarya Sarfare <aarya.work005@gmail.com>
2026-04-01 15:31:38 +0530

    adding the UpdateContactEntry basemodel to my schemas

--- app/schemas.py
+++ app/schemas.py
@@ -3,3 +3,8 @@ from pydantic import BaseModel
 class ContactEntry(BaseModel):
     contact_name : str
     contact_number : int
+
+class UpdateContactEntry(BaseModel):
+    old_contact_name : str
+    new_contact_name : str | None
+    new_contact_number : int | None
