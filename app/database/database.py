d42e82c8943de78660092cd645255575ebfaed65
Aarya-Work <aarya.work005@gmail.com>
2026-04-18 15:33:57 +0530

    removing the unnecessary import

--- app/database/database.py
+++ app/database/database.py
@@ -1,13 +1,10 @@
-import psycopg2
 import os
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 
 env_file_path = ".env.dev"
-
-load_dotenv(dotenv_path=env_file_path)
+load_dotenv(dotenv_path = env_file_path)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
 engine = create_engine(DATABASE_URL, echo=True)
 
