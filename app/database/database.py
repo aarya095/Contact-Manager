417905bddd1a44a3f0493848f61ac44fae960700
Aarya-Work <aarya.work005@gmail.com>
2026-04-18 16:16:04 +0530

    moving the get_db function to database.py

--- app/database/database.py
+++ app/database/database.py
@@ -1,5 +1,6 @@
 import os
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
 env_file_path = ".env.dev"
@@ -7,4 +8,13 @@ load_dotenv(dotenv_path = env_file_path)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL, echo=True)
+
+def get_db():
+    """Dependency to get the database session for testing the API"""
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    try:
+        yield session
+    finally:
+        session.close()
 
