f88737791ee22d5b98eeab98458e40a6ba72988f
Aarya-Work <aarya.work005@gmail.com>
2026-05-02 14:16:55 +0530

    changed the formating for decorator paramters

--- app/routes.py
+++ app/routes.py
@@ -20,8 +20,8 @@ def root():
 
 
 @router.post("/contacts", 
-             status_code=201,
-             summary="Create a new contact")
+             status_code = 201,
+             summary = "Create a new contact")
 def create_contact(
                     contact: ContactEntry,
                     db: Session = Depends(get_db)
@@ -49,7 +49,7 @@ def create_contact(
             )
     
 @router.get("/contacts/{contact_name}",
-            status_code=200, 
+            status_code = 200, 
             summary = "Gets the contact entry by name")
 def get_one_contact_entry(
                         contact_name: str,
@@ -76,7 +76,7 @@ def get_one_contact_entry(
 
 
 @router.get("/contacts", 
-            status_code=200,
+            status_code = 200,
             summary = "Gets all the contact entries stored in the database")
 def get_all_contact_entries(db: Session = Depends(get_db)) -> dict:
     
@@ -90,8 +90,8 @@ def get_all_contact_entries(db: Session = Depends(get_db)) -> dict:
     
 
 @router.put("/contacts", 
-            status_code=200,
-            summary="Updates an existing contact")
+            status_code = 200,
+            summary = "Updates an existing contact")
 def update_contact(
                    contact: UpdateContactEntry,
                    db: Session = Depends(get_db)
@@ -121,8 +121,8 @@ def update_contact(
     
     
 @router.delete("/contacts", 
-               status_code=200,
-               summary="Deletes an existing contact")
+               status_code = 200,
+               summary = "Deletes an existing contact")
 def delete_contact(
                     contact: DeleteContactEntry,
                     db: Session = Depends(get_db)
