a0392027fff1f846b582968684506b169e3e9b32
Aarya-Work <aarya.work005@gmail.com>
2026-05-07 18:40:59 +0530

    droping the unique constraints on name and number which I previous had applied

---
+++ alembic/versions/96293ddbdffd_drop_unique_constraint.py
@@ -0,0 +1,28 @@
+"""drop_unique_constraint
+
+Revision ID: 96293ddbdffd
+Revises: 
+Create Date: 2026-05-07 18:40:04.762133
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '96293ddbdffd'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
