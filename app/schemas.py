f7850c2c332e50284724d7c15e09f83724b0e47f
Aarya-Work <aarya.work005@gmail.com>
2026-05-07 14:08:45 +0530

    altering the schemas for better maintainability

--- app/schemas.py
+++ app/schemas.py
@@ -1,15 +1,14 @@
 from pydantic import BaseModel
 
-class Contact(BaseModel):
-    contact_id: int
-
-class ContactEntry(BaseModel):
+class ContactBase(BaseModel):
     contact_name : str
     contact_number : int
 
-class UpdateContactEntry(Contact):
-    new_contact_name: str | None
-    new_contact_number: int | None
+class ContactCreate(ContactBase):
+    pass
 
-class ContactResponse(Contact):
-    contact_name : str
+class ContactUpdate(ContactBase):
+    pass
+
+class ContactResponse(ContactBase):
+    contact_id: int
