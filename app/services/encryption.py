bec4778d32853621a73d0ed36e18424465fca973
Aarya Sarfare <aarya.work005@gmail.com>
2026-05-26 13:55:31 +0530

    refactor: remove key parameter from encrypt and decrypt functions to use centralized MASTER_KEY

--- app/services/encryption.py
+++ app/services/encryption.py
@@ -3,20 +3,20 @@ from config.config import config
 # Third Party Modules
 from cryptography.fernet import Fernet
 
-def encrypt(contact_number: int, key = config.MASTER_KEY) -> bytes:
+def encrypt(contact_number: int) -> bytes:
     """Encrypts the contact number using fernet a symmmetric cipher"""
 
     contact_number = contact_number.to_bytes(8,'big')   
     
-    f = Fernet(key)
+    f = Fernet(config.MASTER_KEY)
     encrypted_contact_number = f.encrypt(contact_number)
 
-    return encrypted_contact_number, key
+    return encrypted_contact_number
 
-def decrypt(encrypted_contact_number: bytes, key: bytes = config.MASTER_KEY) -> int:
+def decrypt(encrypted_contact_number: bytes) -> int:
     """Decrypts the contact number using fernet a symmmetric cipher"""
 
-    f = Fernet(key)
+    f = Fernet(config.MASTER_KEY)
     original_contact_number_bytes = f.decrypt(encrypted_contact_number)
     original_contact_number = (
         int.from_bytes(original_contact_number_bytes, 'big')
