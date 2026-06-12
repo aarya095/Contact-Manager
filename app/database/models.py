48dc6f26e2cbfe5a90d9490c1b81b8aecc4e8f1d
Aarya Sarfare <aarya.work005@gmail.com>
2026-06-12 13:42:19 +0530

    fix: correct owner_id spelling in Contact model and reorganize User model definition

--- app/database/models.py
+++ app/database/models.py
@@ -1,9 +1,17 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String, ForeignKey
 
 from app.database.database import engine
 
 class Base(DeclarativeBase):
     pass
+
+class User(Base):
+    __tablename__ = "users"
+
+    user_id : Mapped[int] = mapped_column(primary_key=True)
+    username : Mapped[str]
+    password_hash : Mapped[str] = mapped_column(String(50)) 
 
 class Contact(Base):
     __tablename__ = "contacts"
@@ -11,5 +19,6 @@ class Contact(Base):
     contact_id : Mapped[int] = mapped_column(primary_key=True)
     contact_name : Mapped[str] 
     contact_number : Mapped[bytes] 
+    onwer_id : Mapped[int] = mapped_column(ForeignKey("users.user_id"))
 
 Base.metadata.create_all(engine)
