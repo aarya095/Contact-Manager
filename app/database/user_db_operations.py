5f50796b79af9177838f113bec44a4058a1c75cf
Aarya Sarfare <aarya.work005@gmail.com>
2026-07-02 11:13:22 +0530

    feat: implement create_user function for user registration

---
+++ app/database/user_db_operations.py
@@ -0,0 +1,28 @@
+from app.database.models import User
+
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+def create_user(
+    db: Session,
+    username: str,
+    password_hash: str,
+) -> User:
+    """
+    Creates a new user in the database.
+    """
+    
+    user = User(
+        username=username,
+        password_hash=password_hash,
+    )
+
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    return user
