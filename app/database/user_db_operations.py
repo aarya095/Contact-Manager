7aeb0b50fccdc7e98485dbf741e5840ee0b39572
Aarya Sarfare <aarya.work005@gmail.com>
2026-07-02 13:04:36 +0530

    -

--- app/database/user_db_operations.py
+++ app/database/user_db_operations.py
@@ -1,5 +1,7 @@
 from app.database.models import User
 
+from app.exceptions import UserNotFoundError
+
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
@@ -27,6 +29,21 @@ def create_user(
 
     return user
 
+
+def get_user_by_id(
+        user_id: int, 
+        db: Session
+    ) -> User:
+    """
+    Retrieve a user by their ID.
+    """
+
+    statement = select(User).where(User.user_id == user_id)
+    user = db.scalar(statement)
+
+    return user
+
+
 def check_user_exists(
         user_id: int, 
         db: Session
@@ -35,8 +52,8 @@ def check_user_exists(
     Check whether a user exists in the database.
     """
 
-    stmt = select(User.user_id).where(User.user_id == user_id)
-    user_to_find = db.execute(stmt).first()
+    statement = select(User.user_id).where(User.user_id == user_id)
+    user_to_find = db.execute(statement).first()
 
     if user_to_find:
         logger.info(f"Contact found in the database: {user_id}")
