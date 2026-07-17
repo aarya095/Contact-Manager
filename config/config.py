1b3b475c5cf5d8ad2318e3495c5e174ebcd8ef06
Aarya Sarfare <aarya.work005@gmail.com>
2026-07-17 14:17:12 +0530

    doc: add environment variable configurations to Config class in config.py

--- config/config.py
+++ config/config.py
@@ -7,5 +7,8 @@ load_dotenv(dotenv_path = env_file_path)
 class Config:
     DATABASE_URL = os.getenv("DATABASE_URL")
     MASTER_KEY = os.getenv("MASTER_KEY")
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    ALGORITHM = os.getenv("ALGORITHM")
+    ACCESS_TOKEN_EXPIRE_TIME = os.getenv("ACCESS_TOKEN_EXPIRE_TIME")
 
 config = Config()
