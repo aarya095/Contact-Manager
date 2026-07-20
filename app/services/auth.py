2151f3337cc683ca5ad00c00c5ce78fb10a9bc71
Aarya Sarfare <aarya.work005@gmail.com>
2026-07-20 14:25:59 +0530

    refactor: reorganize imports and enhance authenticate_user function

--- app/services/auth.py
+++ app/services/auth.py
@@ -1,13 +1,19 @@
+# Built-in Modules
+from datetime import datetime, timedelta, UTC
+
 # User-Defined Modules
 from config.config import config
+from database.user_db_operations import get_user_by_username
 
 # External Modules
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
 from pydantic import BaseModel
-from datetime import datetime, timedelta, UTC
 from jose import JWTError, jwt
 from pwdlib import PasswordHash
+
+from sqlalchemy.orm import Session
 
 
 password_context = PasswordHash.recommended()
@@ -19,3 +25,17 @@ def verify_password(plain_password: str, hashed_password: str):
 def get_password_hash(plain_password: str) -> str:
     return password_context.hash(plain_password)
 
+def authenticate_user(
+        username: str, 
+        password: str,
+        db: Session,
+        ):
+
+    user = get_user_by_username(db, username)
+    if not user:
+        return False
+        
+    if not verify_password(password, user['hashed_password']):
+        return False
+    
+    return user
