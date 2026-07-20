792d64f615067cc8edfcbc5665a54fc47e83de76
Aarya Sarfare <aarya.work005@gmail.com>
2026-07-20 14:03:29 +0530

    improve: transferred password field to UserCreate and created UserResponse schema

--- app/schemas.py
+++ app/schemas.py
@@ -3,10 +3,12 @@ from pydantic import BaseModel
 
 class UserBase(BaseModel):
     username : str
-    password : str
 
 class UserCreate(UserBase):
-    pass
+    password : str
+
+class UserResponse(UserBase):
+    user_id : int
 
 class ContactBase(BaseModel):
     owner_id : int
