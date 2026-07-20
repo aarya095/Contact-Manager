06214326bb1fd78e6546d286e6848a79e2f18db2
Aarya Sarfare <aarya.work005@gmail.com>
2026-07-20 14:04:06 +0530

    feat: implement password hashing functions for secure authentication

--- app/services/auth.py
+++ app/services/auth.py
@@ -1,6 +1,21 @@
+# User-Defined Modules
+from config.config import config
+
+# External Modules
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from datetime import datetime, timedelta, UTC
 from jose import JWTError, jwt
 from pwdlib import PasswordHash
+
+
+password_context = PasswordHash.recommended()
+oauth_2_scheme = OAuth2PasswordBearer(tokenUrl = "token")
+
+def verify_password(plain_password: str, hashed_password: str):
+    return password_context.verify(plain_password, hashed_password)
+
+def get_password_hash(plain_password: str) -> str:
+    return password_context.hash(plain_password)
+
