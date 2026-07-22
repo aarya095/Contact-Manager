79226e981a0ee215c961f96233cdb0baed3774f6
Aarya Sarfare <aarya.work005@gmail.com>
2026-07-22 13:48:43 +0530

    test: add test for generating different ciphertexts for the same input

--- tests/test_encryption.py
+++ tests/test_encryption.py
@@ -3,12 +3,18 @@ import pytest
 from app.services import encryption 
 
 def test_encryption_decryption_process():
-    """Tests the encryption and decryption process"""
+
     contact_info = 989463872
     encrypted_contact_info = encryption.encrypt(contact_info)
     original_contact_info = encryption.decrypt(encrypted_contact_info)
 
     assert original_contact_info == contact_info
 
-if __name__ == '__main__':
-    test_encryption_decryption_process()
+def test_encrypt_generates_different_ciphertexts():
+    
+    contact_number = 9876543210
+
+    encrypted1 = encryption.encrypt(contact_number)
+    encrypted2 = encryption.encrypt(contact_number)
+
+    assert encrypted1 != encrypted2
